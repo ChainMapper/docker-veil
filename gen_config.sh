0c757c1dea5f3d82700ea8d48f9365215c30976d
Jos Hendriks <jos@hendriks.software>
2018-12-29 22:12:44 +0100

    updated config

--- gen_config.sh
+++ gen_config.sh
@@ -2,10 +2,13 @@
 
 cat  << EOF
 testnet=1
+server=1
 rpcuser=$USER
 rpcpassword=$PASSWORD
 rpcallowip=$RPCALLOW
+rpcbind=0.0.0.0
 rpcport=6666
 txindex=1
-maxconnetions=50
+addnode=159.69.223.84
+addnode=veil.suprnova.cc
 EOF
